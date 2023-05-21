d43329c7e48a46ea68960609ffe61993ab6433b3
maksimwest00 <maksim.west00@yandex.ru>
2023-05-21 15:37:31 +0300

    cleaning code and switching to json

---
+++ Steam Account Switcher/Models/Model.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Steam_Account_Switcher
+{
+    public class SettingsJson
+    {
+        public Settings settings { get; set; }
+    }
+
+    public class Settings
+    {
+        public string path { get; set; }
+    }
+
+    public class Account
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Steam Account Switcher/Service/JsonFileService.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Steam_Account_Switcher
+{
+    public class JsonFileService : IFileService
+    {
+        public void Save<T>(string filename, T data)
+        {
+            File.WriteAllText(filename, JsonConvert.SerializeObject(data, Formatting.Indented));
+        }
+
+        T IFileService.Read<T>(string fileName)
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(fileName));
+        }
+    }
+}
