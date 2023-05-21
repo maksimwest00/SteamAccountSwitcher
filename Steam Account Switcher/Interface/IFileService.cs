8e86cb32f35f1212d00b5b004803fa80fada9917
maksimwest00 <maksim.west00@yandex.ru>
2023-05-21 15:37:31 +0300

    cleaning code and switching to json

---
+++ Steam Account Switcher/Interface/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Steam_Account_Switcher
+{
+    public interface IFileService
+    {
+        T Read<T>(string filename);
+        void Save<T>(string filename, T accounts);
+    }
+}
